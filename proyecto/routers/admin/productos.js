ef3f15bed56d18e4154cfea8d71bef259b5b1136
Eugenia <costamariaeugenia1@gmail.com>
2022-06-24 16:36:01 -0300

    update productos.js

--- proyecto/routers/admin/productos.js
+++ proyecto/routers/admin/productos.js
@@ -1,47 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const connection = require('../controllers/admin/productos');
+const controller = require('../../controllers/admin/productos');
 
-// Para mostrar el listado de los productos
-router.get('/productos', (req, res) => {
-    connection.query('SELECT * FROM productos',  (error, results) => { // para traer los datos desde la base de datos
-      if (error) { throw error }
-  
-      res.render('productos/index', { productos: results });
-    })
-});
+router.get('/productos', controller.index);
 
-// para crear un producto
-router.get('/productos/create', (req, res) => {
-  res.render('productos/create');
-});
+router.get('/productos/create', controller.create);
+router.post('/productos/store', controller.store);
 
-// para insertar el producto creado en la base de datos
-router.post('productos/store', (req,res) => {
-  connection.query('INSERT INTO productos SET = ?', { 
-    // como tengo en la base de datos : aca puede variar
-    codigo: req.body.codigo,
-    nombre: req.body.nombre,
-    descripcion: req.body.descripcion,
-    precio: req.body.precio,
-    categoria_id: req.body.categoria
-  }, (error, results) => {
-    if (error) { throw error }
+router.get('/productos/:codigo', controller.show);
 
-    res.redirect('/productos'); // una vez que ingreso el producto redirijo a la lista de los productos
-  });
-})
+router.get('/productos/:codigo/edit', controller.edit);
+router.put('/productos/update', controller.update);
 
-// Para mostrar el detalle del producto por codigo
-router.get('/productos/:codigo', (req, res) => {
-    connection.query('SELECT * FROM productos WHERE codigo = ?', 
-      [ req.params.codigo ],
-      (error, results) => {
-        if (error) { throw error }
-
-        res.render('productos/show', { producto: results[0] });
-      });
-});
+router.delete('/productos/:codigo/delete', controller.delete);
 
 module.exports = router;
