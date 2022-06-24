997d1a6ffc0bbe1e219c18701b8984d74388572f
Eugenia <costamariaeugenia1@gmail.com>
2022-06-24 15:56:42 -0300

    update app.js with login

--- proyecto/app.js
+++ proyecto/app.js
@@ -4,6 +4,13 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const methodOverride = require('method-override');
+const session = require('express-session');
+
+app.use(session({
+    secret: 'k(x=zN;86;K(Ea>4',
+    resave: false,
+    saveUninitialized: false
+}));
 
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
@@ -12,11 +19,22 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(express.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
-const session = require('express-session');
+
+const isLogin = (req, res, next) => {
+    if (!req.session.user_id) {
+        res.redirect('/login');
+    }
+
+    next();
+}
 
 app.use(require('./routers/index'));
 app.use(require('./routers/productos'));
 app.use(require('./routers/contacto'));
+
+app.use('/admin', isLogin, require('./routes/admin/productos'));
+
+app.use(require('./routes/auth'));
 
 app.use((req, res, next) => {
     res.status(404).send('Not found');
