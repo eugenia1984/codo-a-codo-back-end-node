f47d9bb4c3ff05c8c23370d61b9cc3fbbe64105d
Eugenia <costamariaeugenia1@gmail.com>
2022-06-24 16:44:21 -0300

    update productos.js

--- proyecto/controllers/admin/productos.js
+++ proyecto/controllers/admin/productos.js
@@ -4,13 +4,13 @@ module.exports.index = (req, res) => {
   connection.query('SELECT * FROM productos',  (error, results) => {
     if (error) { throw error }
 
-    res.render('admin/productos/index', { productos: results });
+    res.render('admin/productos/index', { productos: results });  // res.render('admin/productos/index', { productos: results, layout: 'layout-admin' });
   })
 }
 
 // para crear un nuevo producto
 module.exports.create = (req, res) => {
-  res.render('admin/productos/create');
+  res.render('admin/productos/create'); //  res.render('admin/productos/create', { layout: 'layout-admin' });
 }
 
 module.exports.store =  (req,res) => {
@@ -18,33 +18,30 @@ module.exports.store =  (req,res) => {
     codigo: req.body.codigo,
     nombre: req.body.nombre,
     descripcion: req.body.descripcion,
-    categoria_id: req.body.categoria
+    categoria_id: req.body.categoria,
+    precio : req.body.precio
   }, (error, results) => {
     if (error) { throw error }
 
-    res.redirect('/admin/producto');
+    res.redirect('/admin/productos');
   });
 }
 
 // para mostrar un producto por su codigo
 module.exports.show = (req, res) => {
-  connection.query('SELECT * FROM productos WHERE codigo = ?',
-    [ req.params.codigo],
-    (error, results) => {
+  connection.query('SELECT * FROM productos WHERE codigo = ?', [ req.params.codigo], (error, results) => {
       if (error) { throw error }
 
-      res.render('productos/show', { productos: results[0] })
+      res.render('admin/productos/show', { productos: results[0] }) //  res.render('admin/productos/show', { producto: results[0], layout: 'layout-admin' });
   });
 }
 
 // para modificar un producto de la base de datos
 module.exports.edit = (req,res) => {
-  connection.query('SELECT * FROM productos WHERE codigo = ?',
-    [ req.params.codigo],
-    (error, results) => {
+  connection.query('SELECT * FROM productos WHERE codigo = ?', [ req.params.codigo], (error, results) => {
       if (error) { throw error }
       
-      res.render('admin/productos/edit', { producto: results[0] });
+      res.render('admin/productos/edit', { producto: results[0] }); //   res.render('admin/productos/edit', { producto: results[0], layout: 'layout-admin' });
   });
 }
 
@@ -52,7 +49,8 @@ module.exports.update = (req, res) => {
   connection.query('UPDATE productos SET ? WHERE codigo = ?', [ {
     nombre: req.body.nombre,
     descripcion : req.body.descripcion,
-    categoria_id: req.body.categoria // base de datos : formulario
+    categoria_id: req.body.categoria, // base de datos : formulario
+    precio: req.body.precio
   }, req.body.codigo], (error, results) => {
     if(error) { throw error }
 
